e14196424004f1e7c46d89c3d83b87f4d31fe566
Gentry Rolofson <xdarkicex@gmail.com>
2015-04-13 01:26:29 -0700

    objects

---
+++ assets/js/Objects.js
@@ -0,0 +1,10 @@
+//Object creation
+
+var player = new Object();
+player.name = "Fred";
+player.score = 10000;
+player.rank = 1;
+
+//Shorthand
+
+var player1 = { name: "Fred", score: 10000, rank: 1};
